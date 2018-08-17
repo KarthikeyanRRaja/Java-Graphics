d75eb87b33415f5441e563c74be7e6c25d9e91f2
KarthikeyanRRaja <40626199+KarthikeyanRRaja@users.noreply.github.com>
2018-08-17 14:13:35 +0530

    Add files via upload

---
+++ circle.java
@@ -0,0 +1,25 @@
+
+import java.awt.Graphics;
+	import java.applet.Applet;
+	/*<applet code="circle" width="500" height="400">
+	</applet>*/
+
+	
+	public class circle extends Applet
+	{
+	  public void paint (Graphics g)
+	  {			                 // using drawRoundRect()
+	    g.drawRoundRect(40, 50, 90, 90, 200, 200);
+	   // g.fillRoundRect(40, 160, 90, 90, 200, 200);
+
+				                // using drawArc()
+	    g.drawArc(200, 400, 100, 80, 0, 180);
+	g.drawLine(200,60,300,60);
+	    //g.fillArc(, 160, 90, 90, 0, 360);
+	  }
+	
+	public static void main(String ar[])
+	{
+		
+	}
+	}
