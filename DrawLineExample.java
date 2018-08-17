f424c663ca23c9c1a1b40192180b965fec48aa60
KarthikeyanRRaja <40626199+KarthikeyanRRaja@users.noreply.github.com>
2018-08-17 14:14:35 +0530

    Add files via upload

---
+++ DrawLineExample.java
@@ -0,0 +1,34 @@
+import java.applet.Applet;
+import java.awt.Graphics;
+import java.applet.*;
+import java.awt.*;
+
+ 
+ 
+public class DrawLineExample extends Applet{
+ 
+	public void paint(Graphics g){
+		
+		/*
+		 * to draw line in an applet window use,
+		 * void drawLine(int x1,int y1, int x2, int y2)
+		 * method.
+		 * 
+		 * This method draws a line between (x1,y1) and (x2,y2)
+		 * coordinates in a current color.
+		 */
+		
+		//this will draw a line between (10,10) and (50,50) coordinates. 
+		g.drawRect(10,10,50,50);
+		g.setColor(Color.red);
+		
+		//draw vertical line
+		g.drawLine(10,50,10,100);
+		
+		//draw horizontal line
+		g.drawLine(10,10,50,10);
+	}
+public static void main(String ar[])
+{
+}
+}
