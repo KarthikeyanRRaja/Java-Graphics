56d228324a4b41cd8fd35769227b32831f6ff901
KarthikeyanRRaja <40626199+KarthikeyanRRaja@users.noreply.github.com>
2018-09-11 20:57:51 +0530

    Add files via upload

---
+++ First.java
@@ -0,0 +1,10 @@
+	import java.applet.Applet;  
+	import java.awt.Graphics;  
+	public class First extends Applet{  	  
+	public void paint(Graphics g){  
+	g.drawString("welcome to applet",150,150);  
+	}
+	public static void main(String ar[])
+{
+}  	  
+	}  
